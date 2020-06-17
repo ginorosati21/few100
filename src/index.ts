73337e8131898a0a9343d0089010ace9721f8b48
ginorosati21 <ginorosati21@gmail.com>
2020-06-17 17:16:07 +0000

    1:15 Wednesday

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
-console.log('Testing!');
 
+const tipPercentage = .20;
+
+const btnCalculate = document.getElementById('btnCalculate');
+const txtAmount = document.getElementById('txtAmount') as HTMLInputElement;
+const spnBillAmount = document.getElementById('spnBillAmount') as HTMLSpanElement;
+const spnTipPercentage = document.getElementById('spnTipPercentage') as HTMLSpanElement;
+const spnTipAmount = document.getElementById('spnTipAmount') as HTMLSpanElement;
+const spnTotal = document.getElementById('spnTotal') as HTMLSpanElement;
+
+console.log({ btnCalculate, txtAmount });
+
+btnCalculate.addEventListener('click', handleCalculate);
+
+function handleCalculate() {
+    const amount = txtAmount.valueAsNumber;
+    spnBillAmount.innerText = amount.toString();
+    spnTipPercentage.innerText = (tipPercentage * 100).toString() + '%';
+    const tipAmount = amount * tipPercentage;
+    spnTipAmount.innerText = tipAmount.toString();
+    const total = amount + tipAmount;
+    spnTotal.innerText = total.toString();
+    console.log(amount);
+    // txtAmount.value = '';
+    txtAmount.select();
+    txtAmount.focus();
+}
